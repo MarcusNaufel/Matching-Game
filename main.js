cdbda04db2cfd7a0e7169fcfc93bdd8bed4fff16
MarcusNaufel <82543159+MarcusNaufel@users.noreply.github.com>
2021-07-12 20:10:23 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import "./src/styles/settings/colors.css";
 import "./src/styles/generic/reset.css";
 import "./src/styles/elements/base.css";
+
 import BoardGame from "./src/objects/BoardGame";
-import PlayerName from "./src/components/PlayerName";
 import ScoreBoard from "./src/objects/ScoreBoard";
 
 const $root = document.querySelector("#root");
@@ -11,6 +11,6 @@ $root.insertAdjacentHTML(
   "beforeend",
   `
     ${ScoreBoard()}
-    ${BoardGame(6)}
+    ${BoardGame(4)}
   `
 );
